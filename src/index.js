5f34cc93e7e96992c6a46c12ebe4d47e2766ce7d
Hemant Chavan <hemant.chavan1@gmail.com>
2019-02-26 10:35:37 +0530

    to learn sync repo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+/* Adding comment to learn syncing the repositories */
 
   function Square(props) {
     return (
